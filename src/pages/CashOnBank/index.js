2a1a450165b8658a5dfd04f81b747ae01206cd8d
ferrenkalalo <s21910001@student.unklab.ac.id>
2022-04-04 11:33:02 +0800

    update 3 latest transaction

--- src/pages/CashOnBank/index.js
+++ src/pages/CashOnBank/index.js
@@ -2,6 +2,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Button, Header, TextInput} from '../../components';
 import Gap from '../../components/atoms/Gap';
+import Transaction from '../../components/molecules/Transaction';
 
 const CashOnBank = ({navigation}) => {
   return (
@@ -12,7 +13,7 @@ const CashOnBank = ({navigation}) => {
           navigation.goBack();
         }}
       />
-      <Gap height={24} />
+      <Gap height={15} />
       <View style={styles.container}>
         <TextInput title="Description" placeholder="Add the description" />
         <Gap height={17} />
@@ -20,11 +21,11 @@ const CashOnBank = ({navigation}) => {
         <Gap height={22} />
         <Button title="Save" />
       </View>
-      <Gap height={24} />
+      <Gap height={10} />
       <View style={styles.container}>
         <Text style={styles.Text}>Last 3 Transactions</Text>
         <Gap height={10} />
-        <View style={styles.box}>
+        {/* <View style={styles.box}>
           <Text style={styles.subText}>Transaction 1</Text>
         </View>
         <Gap height={10} />
@@ -34,7 +35,23 @@ const CashOnBank = ({navigation}) => {
         <Gap height={10} />
         <View style={styles.box}>
           <Text style={styles.subText}>Transaction 3</Text>
-        </View>
+        </View> */}
+        <Transaction
+          tanggal={'17 april 2020'}
+          keterangan={'Water, Food'}
+          jumlah={'- Rp.300.000'}
+        />
+        <Transaction
+          tanggal={'18 april 2020'}
+          keterangan={'Office Supplies'}
+          jumlah={'- Rp.300.000'}
+        />
+        <Transaction
+          tanggal={'19 april 2020'}
+          keterangan={'Top Up'}
+          jumlah={'+ Rp.300.000'}
+          color="#02CF8E"
+        />
       </View>
     </View>
   );
